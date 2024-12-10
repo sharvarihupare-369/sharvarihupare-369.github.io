0a059875b008292269d5e0a49f5d88d193315a6b
sharvarihupare-369 <sharvarihupare15@gmail.com>
2024-12-10 22:28:30 +0530

    id is added to github and likedin in footer section

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,26 +1,57 @@
-import { Box, Flex } from '@chakra-ui/react'
-import React from 'react'
-import { FaGithub, FaHeart, FaLinkedin } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import { FaGithub, FaHeart, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <Flex  gap="15px" boxSizing='border-box' alignItems={'center'} flexDirection={{base:"column",sm:"column",md:"column",lg:"row",xl:"row","2xl":"row"}} color={'gray.300'} fontSize={'20px'} bg={'black'} p={'10px'} alignItems={'center'} justifyContent="space-between">
-    <Box   fontWeight={'bold'}  color={'#0ef'}>{'< S/>'}</Box>
-    <Box display={"flex"} alignItems={"center"}>Made with <span> <FaHeart color='red' style={{margin:"0 5px"}} /></span> by <span style={{color : '#0ef',marginLeft:"3px"}}> Sharvari Hupare !</span></Box>
-     <Flex alignItems={'center'}  >
-     
-        <Link to="https://github.com/sharvarihupare-369"  >
-            <FaGithub  fontSize={"35px"}/>
+    <Flex
+      gap="15px"
+      boxSizing="border-box"
+      alignItems={"center"}
+      flexDirection={{
+        base: "column",
+        sm: "column",
+        md: "column",
+        lg: "row",
+        xl: "row",
+        "2xl": "row",
+      }}
+      color={"gray.300"}
+      fontSize={"20px"}
+      bg={"black"}
+      p={"10px"}
+      justifyContent="space-between"
+    >
+      <Box fontWeight={"bold"} color={"#0ef"}>
+        {"< S/>"}
+      </Box>
+      <Box display={"flex"} alignItems={"center"}>
+        Made with{" "}
+        <span>
+          {" "}
+          <FaHeart color="red" style={{ margin: "0 5px" }} />
+        </span>{" "}
+        by{" "}
+        <span style={{ color: "#0ef", marginLeft: "3px" }}>
+          {" "}
+          Sharvari Hupare !
+        </span>
+      </Box>
+      <Flex alignItems={"center"}>
+        <Link id="contact-github" to="https://github.com/sharvarihupare-369">
+          <FaGithub fontSize={"35px"} />
         </Link>
-        <Link to="https://www.linkedin.com/in/sharvari-hupare15/"  >
-            <FaLinkedin style={{marginLeft:"20px"}} color='#0e76a8' fontSize={"35px"}/>
+        <Link id="contact-linkedin" to="https://www.linkedin.com/in/sharvari-hupare15/">
+          <FaLinkedin
+            style={{ marginLeft: "20px" }}
+            color="#0e76a8"
+            fontSize={"35px"}
+          />
         </Link>
+      </Flex>
+    </Flex>
+  );
+};
 
-        
-     </Flex>
- </Flex>
-  )
-}
-
-export default Footer
+export default Footer;
